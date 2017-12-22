8eee1bddd2424e1c68c87cd538e28a5d2f5c6c31
Jordan Castelloe <jordan.castelloe@gmail.com>
2017-12-22 07:51:49 -0500

    added dark theme and large text

--- scripts/eventHandler.js
+++ scripts/eventHandler.js
@@ -5,14 +5,17 @@ let loader = require("./loadData.js");
 const clearAll = require("./clearAll.js");
 const messageMaker = require("./messageMaker.js");
 const deleteIndividual = require("./deleteIndividual.js");
+const themeChanger = require("./themeChanger.js");
 
 const sendButton = document.getElementById("send-button");
 const clearButton = document.getElementById("clear-button");
+const themeButton = document.getElementById("theme-button");
+const textSizeButton = document.getElementById("text-size-button");
 const messageTextArea = document.getElementById("message-textarea");
 const messageContainer = document.getElementById("message-area");
 
 module.exports.activateEventListeners = function(){
-    
+
     loader.loadMessages();
 
     sendButton.addEventListener("click", function () {
@@ -35,6 +38,14 @@ module.exports.activateEventListeners = function(){
             let targetMessage = event.target.parentNode;
             deleteIndividual.deleteThisMessage(targetMessage);
         }
+    });
+
+    themeButton.addEventListener("click", function(){
+        themeChanger.changeTheme();
+    });
+
+    textSizeButton.addEventListener("click", function(){
+        themeChanger.changeTextSize();
     });
 
     
