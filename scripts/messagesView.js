bea7f44c4c5e1f1af128325284fee120966cbdf1
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-01-01 17:14:07 -0600

    typing indicator css

--- scripts/messagesView.js
+++ scripts/messagesView.js
@@ -2,6 +2,7 @@
 
 module.exports.printSection = function (section) {
     for (let i = 0; i < section.messages.length; i++) {
+        createTypingIndicator();
         createMessageDiv(section.messages[i].text, section.messages[i].name);
     }
 
@@ -16,26 +17,32 @@ module.exports.printSection = function (section) {
 
 module.exports.printTruth = function () {
     let messageText = $(".truth-textarea").val();
-    console.log('this should be the truth', messageText);
     printPlayerMessage(messageText);
 
 };
 
 module.exports.printLie = function () {
     let messageText = $(".lie-textarea").val();
-    console.log('this should be the lie', messageText);
     printPlayerMessage(messageText);
 };
 
 function printPlayerMessage (text) {
     createMessageDiv(text, "You");
-    // save message
     clearTextArea();
 }
 
 
 function createMessageDiv(text, character) {
     let messageDiv = $("<div>", { class: `message-div ${character}` }).text(`${character}: ${text}`).appendTo($("#message-area"));
+}
+
+function createTypingIndicator(){
+    console.log("create typing indicator function fired");
+    let typingIndicator = $("<div>", {class: 'typing-indicator'}).appendTo($('#message-area'));
+    let dotOne = $("<span>").appendTo(typingIndicator);
+    let dotTwo = $("<span>").appendTo(typingIndicator);
+    let dotThree = $("<span>").appendTo(typingIndicator);
+
 }
 
 
