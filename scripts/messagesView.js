b492ce0ea178a3d5b69e7e1f6b460e305ab6ef98
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-01-01 17:43:18 -0600

    typing indicator fires but now player messages don't print

--- scripts/messagesView.js
+++ scripts/messagesView.js
@@ -1,18 +1,30 @@
 'use strict';
 
 module.exports.printSection = function (section) {
-    for (let i = 0; i < section.messages.length; i++) {
-        createTypingIndicator(section.messages[i].name);
-        createMessageDiv(section.messages[i].text, section.messages[i].name);
-    }
+    let typingIndicator = "";
+    let messageDiv = "";
 
+    for (let i = 0; i < section.messages.length; i++) {
+        typingIndicator = createTypingIndicator(section.messages[i].name);
+        messageDiv = createMessageDiv(section.messages[i].text, section.messages[i].name);
+        typingIndicator.appendTo($("#message-area"));
+        setTimeout(switchMessageDiv, 1000); 
+    }
     $('.narration').text(section.narration);
     $('.truth-text').text(section.options.truth.truthPrompt);
     $('.truth-textarea').attr('placeholder', section.options.truth.truthDefault);
     $('.lie-text').text(section.options.lie.liePrompt);
     $('.lie-textarea').attr('placeholder', section.options.lie.lieDefault);
     $('#character-list').text(section.characters.join(', '));
+
+  
+    function switchMessageDiv() {
+        typingIndicator.remove();
+        messageDiv.appendTo($("#message-area"));
+
+    }
 };
+
 
 
 module.exports.printTruth = function () {
@@ -33,16 +45,17 @@ function printPlayerMessage (text) {
 
 
 function createMessageDiv(text, character) {
-    let messageDiv = $("<div>", { class: `message-div ${character}` }).text(`${character}: ${text}`).appendTo($("#message-area"));
+    let messageDiv = $("<div>", { class: `message-div ${character}` }).text(`${character}: ${text}`);
+    return messageDiv;
 }
 
 function createTypingIndicator(character){
     console.log("create typing indicator function fired");
-    let typingIndicator = $("<div>", {class: `typing-indicator message-div ${character}`}).appendTo($('#message-area'));
+    let typingIndicator = $("<div>", {class: `typing-indicator message-div ${character}`});
     let dotOne = $("<span>").appendTo(typingIndicator);
     let dotTwo = $("<span>").appendTo(typingIndicator);
     let dotThree = $("<span>").appendTo(typingIndicator);
-
+    return typingIndicator;
 }
 
 
